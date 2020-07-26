733e992fddb239d299c4ae2f70599d2b9698e6a7
Lucas Marinho <marinholucasit@gmail.com>
2020-07-26 12:32:51 -0300

    including validations

--- lib/ui/contact_page.dart
+++ lib/ui/contact_page.dart
@@ -19,6 +19,8 @@ class _ContactPageState extends State<ContactPage> {
   final _emailController = TextEditingController();
   final _numberController = TextEditingController();
 
+  final _nameFocus = FocusNode();
+
   @override
   void initState() {
     super.initState();
@@ -35,66 +37,107 @@ class _ContactPageState extends State<ContactPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.red,
-        title: Text(
-          _editedContact.name ?? "New Contact",
+    return WillPopScope(
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.red,
+            title: Text(
+              _editedContact.name ?? "New Contact",
+            ),
+            centerTitle: true,
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              if (_editedContact.name != null &&
+                  _editedContact.name.isNotEmpty) {
+                Navigator.pop(context, _editedContact);
+              } else {
+                FocusScope.of(context).requestFocus(_nameFocus);
+              }
+            },
+            child: Icon(Icons.save),
+            backgroundColor: Colors.blueGrey,
+          ),
+          body: SingleChildScrollView(
+            padding: EdgeInsets.all(10.0),
+            child: Column(
+              children: <Widget>[
+                GestureDetector(
+                  child: Container(
+                    width: 140.0,
+                    height: 140.0,
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                            image: _editedContact.img != null
+                                ? FileImage(File(_editedContact.img))
+                                : AssetImage("images/user.png"))),
+                  ),
+                ),
+                TextField(
+                  focusNode: _nameFocus,
+                  controller: _nameController,
+                  decoration: InputDecoration(labelText: "Name"),
+                  onChanged: (text) {
+                    _userEdited = true;
+                    setState(() {
+                      _editedContact.name = text;
+                    });
+                  },
+                ),
+                TextField(
+                  controller: _emailController,
+                  decoration: InputDecoration(labelText: "Email"),
+                  onChanged: (text) {
+                    _userEdited = true;
+                    _editedContact.email = text;
+                  },
+                  keyboardType: TextInputType.emailAddress,
+                ),
+                TextField(
+                  controller: _numberController,
+                  decoration: InputDecoration(labelText: "Number"),
+                  onChanged: (text) {
+                    _userEdited = true;
+                    _editedContact.number = text;
+                  },
+                  keyboardType: TextInputType.phone,
+                )
+              ],
+            ),
+          ),
         ),
-        centerTitle: true,
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.save),
-        backgroundColor: Colors.blueGrey,
-      ),
-      body: SingleChildScrollView(
-        padding: EdgeInsets.all(10.0),
-        child: Column(
-          children: <Widget>[
-            GestureDetector(
-              child: Container(
-                width: 140.0,
-                height: 140.0,
-                decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    image: DecorationImage(
-                        image: _editedContact.img != null
-                            ? FileImage(File(_editedContact.img))
-                            : AssetImage("images/user.png"))),
+        onWillPop: null);
+  }
+
+  _requestPop() {
+    if (_userEdited) {
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text("Do you wish to discard the changes?"),
+            content: Text("Changes will be lost"),
+            actions: <Widget>[
+              FlatButton(
+                child: Text("Cancel"),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
               ),
-            ),
-            TextField(
-              controller: _nameController,
-              decoration: InputDecoration(labelText: "Name"),
-              onChanged: (text) {
-                _userEdited = true;
-                setState(() {
-                  _editedContact.name = text;
-                });
-              },
-            ),
-            TextField(
-              controller: _emailController,
-              decoration: InputDecoration(labelText: "Email"),
-              onChanged: (text) {
-                _userEdited = true;
-                _editedContact.email = text;
-              },
-              keyboardType: TextInputType.emailAddress,
-            ),
-            TextField(
-              controller: _numberController,
-              decoration: InputDecoration(labelText: "Number"),
-              onChanged: (text) {
-                _userEdited = true;
-                _editedContact.number = text;
-              },
-              keyboardType: TextInputType.phone,
-            )
-          ],
-        ),
-      ),
-    );
+              FlatButton(
+                child: Text("Yes"),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              )
+            ],
+          );
+        },
+      );
+      return Future.value(false);
+    } else {
+      return Future.value(true);
+    }
   }
 }
